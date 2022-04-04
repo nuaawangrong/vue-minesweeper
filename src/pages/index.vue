8091675eacb720a2bd171a5c3fc9dfa799411f22
nuaawangrong <sszwangrong@126.com>
2022-04-04 17:28:02 +0800

    chore:update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { text } from 'stream/consumers'
-
-const name = $ref('')
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-
-interface BlockState {
-  x: number
-  y: number
-  revealed?: boolean
-  mine?: boolean
-  flagged?: boolean
-  adjacentMines: number
-}
+import type { BlockState } from '~/types'
 
 const WIDTH = 10
 const HEIGHT = 10
 
-const state = reactive(
+const state = ref(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH },
       (_, x): BlockState => ({
@@ -34,13 +17,13 @@ const state = reactive(
 )
 
 function generateMines(initial: BlockState) {
-  for (const row of state) {
+  for (const row of state.value) {
     for (const block of row) {
       if (Math.abs(initial.x - block.x) <= 1)
         continue
       if (Math.abs(initial.y - block.y) <= 1)
         continue
-      block.mine = Math.random() < 0.2
+      block.mine = Math.random() < 0.1
     }
   }
 
@@ -71,7 +54,7 @@ const numberColors = [
 ]
 
 function updateNumbers() {
-  state.forEach((row, y) => {
+  state.value.forEach((row, y) => {
     row.forEach((block, x) => {
       if (block.mine)
         return
@@ -80,7 +63,7 @@ function updateNumbers() {
         const y2 = y + dy
         if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
           return
-        if (state[y2][x2].mine)
+        if (state.value[y2][x2].mine)
           block.adjacentMines += 1
       })
     })
@@ -96,9 +79,9 @@ function expendZero(block: BlockState) {
     const y2 = block.y + dy
     if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
       return
-    if (!state[y2][x2].revealed) {
-      state[y2][x2].revealed = true
-      expendZero(state[y2][x2])
+    if (!state.value[y2][x2].revealed) {
+      state.value[y2][x2].revealed = true
+      expendZero(state.value[y2][x2])
     }
   })
 }
@@ -106,11 +89,20 @@ function expendZero(block: BlockState) {
 let mineGenerated = false
 const dev = false
 
-function onClick(block: BlockState) {
+function onRightClick(block: BlockState) {
+  if (block.revealed)
+    return
+  block.flagged = !block.flagged
+}
+
+function onClick(e: MouseEvent, block: BlockState) {
   if (!mineGenerated) {
     generateMines(block)
     mineGenerated = true
   }
+
+  if (block.flagged)
+    return
 
   block.revealed = true
   expendZero(block)
@@ -119,11 +111,29 @@ function onClick(block: BlockState) {
 }
 
 function getBlockClass(block: BlockState) {
+  if (block.flagged)
+    return 'bg-gray-400/20'
   if (!block.revealed)
-    return 'bg-gray-400/10'
+    return 'bg-gray-400/20 hover:bg-gray/10'
   return block.mine
     ? 'bg-red-500/50'
     : numberColors[block.adjacentMines]
+}
+
+watchEffect(checkGameState)
+
+function checkGameState() {
+  // if (!mineGenerated)
+  //   return
+
+  const blocks = state.value.flat()
+
+  if (blocks.every(block => block.revealed || block.flagged)) {
+    if (blocks.some(block => (block.flagged && !block.mine)))
+      alert('You Cheat!')
+    else
+      alert('You Win!')
+  }
 }
 
 </script>
@@ -144,12 +154,15 @@ function getBlockClass(block: BlockState) {
           flex="~"
           items-center justify-center
           w-10 h-10 m="0.5"
-          hover="bg-gray/10"
           border="1 gray-400/40"
           :class="getBlockClass(block)"
-          @click="onClick(block)"
+          @click="onClick($event, block)"
+          @contextmenu.prevent="onRightClick(block)"
         >
-          <template v-if="block.revealed || dev">
+          <template v-if="block.flagged">
+            <div i-mdi:flag text-red />
+          </template>
+          <template v-else-if="block.revealed || dev">
             <div v-if="block.mine" i-mdi:mine />
             <div v-else>
               {{ block.adjacentMines }}
